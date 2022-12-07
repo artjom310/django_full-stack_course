aaa724ecbc17e5d78e83ad99e223a5ad6b994fff
Artem Frolov <artjom310@gmail.com>
2022-12-07 16:38:25 +0300

    site is done

--- password_gen_proj/password_generator/settings.py
+++ password_gen_proj/password_generator/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'generator',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +122,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

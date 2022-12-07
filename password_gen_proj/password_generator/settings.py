56faa5e1c7f8393dda0249b959906a27dfdff2aa
Artem Frolov <artjom310@gmail.com>
2022-12-07 16:57:54 +0300

    replaced secret key from settings

--- password_gen_proj/password_generator/settings.py
+++ password_gen_proj/password_generator/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87!%3^hy7zr%he)ccy97%0=#5vy@#p5t4ye652l8i2e0m1zf3e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
